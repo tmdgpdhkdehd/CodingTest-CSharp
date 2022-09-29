cb45aa22aac3826d233b318018b182f1c7fc201e
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2022-09-30 00:06:12 +0900

    Create 기능개발.cs

---
+++ Programmers/2Level/기능개발.cs
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds)
+{
+    vector<int> answer;
+    queue<int> q;
+    
+    for(int i=0; i<progresses.size();i++)
+    {
+        if((100-progresses[i])%speeds[i]==0)
+        {
+            q.push((100-progresses[i])/speeds[i]);
+        }
+        else
+        {
+            q.push((100-progresses[i])/speeds[i]+1);
+        }
+    }
+    
+    while(!q.empty())
+    {
+        int cnt=1;
+        int current = q.front();
+        
+        q.pop();
+        
+        while(!q.empty() && q.front()<=current)
+        {
+            cnt++;
+            q.pop();
+        }
+        
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
