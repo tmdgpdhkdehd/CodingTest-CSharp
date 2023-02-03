723c9f9d817f743c45b37028be1cb2646b558560
Ellieka <tmdgpdhkdehd@naver.com>
2023-02-04 00:38:35 +0900

    Update 무인도여행.cs

--- Programmers/2Level/무인도여행.cs
+++ Programmers/2Level/무인도여행.cs
@@ -2,52 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-// 상하좌우 연결되어 있으면 숫자 합하고 합한 곳은 X 표시
-// 더는 연결된 곳이 없으면 합한 숫자는 배열에 저장
-// X표시 된 곳은 건너뛰기
-// maps 끝까지 탐색 마쳤으면 배열을 오름차순 정렬하고 반환
+// 1. 숫자인 곳이 있으면 bfs 실행, X인 곳이면 건너뛰기
+// 2. bfs 실행하면 무한탐색을 막기 위해 해당 위치의 식량 값을 저장하고 해당 위치를 X 표시
+// 3. bfs 실행 중, 상하좌우 중 연결되어 있는 곳(숫자)이 있으면 그 위치에 bfs 실행 반복
+// 4. 더는 연결된 곳이 없으면 누적된 식량 값을 리스트에 저장
+// 맵을 모두 탐색할 때까지 1~4번 반복
+// 맵을 모두 탐색했으면 오름차순으로 정렬한 후 배열로 전환하여 반환
 public class Solution {
-    Queue<(int, int)> q = new Queue<(int, int)>();
-    int colume = 0;
-    int row = 0;
+    int colume = 0;     // 행
+    int row = 0;        // 열
 
     public int bfs(int x, int y, string[] maps)
     {
-        // X라면 건너뛰기
-        if (maps[x][y] == 'X')
-            return 0;
-
+        // 상하좌우
         int[] moveX = {-1, 1, 0, 0};
         int[] moveY = {0, 0, -1, 1};
 
         // 무한반복되지 않게 X로 만들어주고 식량에 더하기
         int food = (int)Char.GetNumericValue(maps[x][y]);
+        // maps[x][y] = 'X'를 하려고 하면
+        // property or indexer `string.this[int]' cannot be assigned to (it is read-only)
+        // 라는 오류가 뜨기 때문에 StringBuilder 사용하여 바꿈
         StringBuilder sb = new StringBuilder(maps[x]);
         sb[y] = 'X';
         maps[x] = sb.ToString();
 
-        q.Enqueue((x, y));
-
-        //while(q.Count != 0)
-        //{
-            (int a, int b) = q.Dequeue();
-
-            for (int i = 0; i < 4; i++)
-            {
-                int nx = a + moveX[i];
-                int ny = b + moveY[i];
-
-                if (nx < 0 || nx >= colume || ny < 0 || ny >= row)
-                    continue;
-                if (maps[nx][ny] == 'X')
-                    continue;
-                else
-                {
-                    food += bfs(nx, ny, maps);
-                    //q.Enqueue((nx, ny));
-                }
-            }
-        //}
+        // 상하좌우에 연결된 섬이 있는지 탐색
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + moveX[i];
+            int ny = y + moveY[i];
+            
+            // 막혀있는 곳은 건너뛰기
+            if (nx < 0 || nx >= colume || ny < 0 || ny >= row)
+                continue;
+            // X인 곳은 건너뛰기
+            if (maps[nx][ny] == 'X')
+                continue;
+            // 섬이 있다면 bfs 실행하고 식량 누적
+            else
+                food += bfs(nx, ny, maps);
+        }
 
         return food;
     }
@@ -61,27 +56,24 @@ public class Solution {
         {
             for (int j = 0; j < row; j++)
             {
-                int food = bfs(i, j, maps);
-                if (food == 0)
-                {
+                // X인 곳은 건너뛰기
+                if (maps[i][j] == 'X')
                     continue;
-                }
-                else
-                {
-                    answer.Add(food);
-                }
+                
+                // 누적된 식량을 리스트에 저장
+                int food = bfs(i, j, maps);
+                answer.Add(food);
             }
         }
 
+        // 리스트에 값이 없다면 섬이 없는 것이므로 -1을 반환하게끔 리스트에 -1 추가
         if (answer.Count == 0)
-        {
             answer.Add(-1);
-        }
+        // 오름차순 정렬
         else
-        {
             answer.Sort();
-        }
 
+        // 리스트를 배열로 전환
         return answer.ToArray();
     }
 }
